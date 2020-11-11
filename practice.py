3b8d87175b16badcf52ca8d2e11609e39d679793
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2020-11-11 14:20:46 -0500

    practice

--- practice.py
+++ practice.py
@@ -191,3 +191,18 @@
 # print (abs(-2.9))  # absolute value of a number
 # print(math.ceil(2.9))  # rounding up
 
+''' Type conversation'''
+
+# x = input("x: ")
+# y = int(x) + 1
+# print(f"x: {x}, y: {y}")
+
+# int(x)
+# float(x)
+# bool(x)
+'''Interesting Falsy boolean values'''
+# ""
+# 0
+# None
+
+# str(x)
