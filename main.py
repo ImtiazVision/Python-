441a4a5943e8290b5a63dd5bb39be4b468cd089b
Imtiaz A <ahmedimtiaz.ahmed@gmail.com>
2020-12-05 02:46:21 +0000

    code

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello World!")
+print("Hello World!\nHello world!!\nHello World!!!")
+
+#Concatination. we can add spaces after hello or before imtiaz or use empty space within a quotation and concatinate them. 
+print("Hello " + " " + " Imtiaz")
 #Write your code below this line 👇
 print('Day 1 - Python Print Function')
 print('The function is declared like this:')
