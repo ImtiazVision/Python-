5113c7ae7fc4d1d1e5291fa7a4bbea10a793aa3f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2020-12-04 21:38:48 -0500

    update

--- 100daysofcode.py
+++ 100daysofcode.py
@@ -11,4 +11,75 @@
 # Then print() will print the word "Hello" and the user input
 # print("Hello " + input("What is your name?"))
 
-print(len(input("What is your name? ")))
+# print(len(input("What is your name? ")))
+
+""" BMI Calculator  """
+
+
+#🚨 Don't change the code below 👇
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+#height_as_float = float(height)
+#weight_as_int = int(weight)
+#bmi = weight_as_int/(height_as_float ** 2)
+
+
+#bmi = int(weight)/(float(height)*float(height))
+# bmi = int(weight) / float(height) ** 2
+# bmi_as_int = int(bmi)
+# print("Your BMI is: " , bmi_as_int)
+
+""" How much time we have left in our life calculator """
+
+
+"""My Code"""
+
+# 🚨 Don't change the code below 👇
+# age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# day = (90 - int(age)) * 365
+# week = (90 - int(age)) * 52
+# month = (90 - int(age)) * 12
+
+# print(f"You have {day} days, {week} weeks, and {month} months left.")
+
+
+""" Instructors code """
+
+# 🚨 Don't change the code below 👇
+# age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# years = 90 - int(age)
+# months = round(years * 12)
+# weeks = round(years * 52)
+# days = round(years * 365)
+
+# print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+# print ("Hello World")
+
+""" Calculator Project """
+# print("Welcome to the tip calculator!")
+# bill = float(input("What was the total bill?\n$"))
+# tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+#If the bill was $150.00, split between 5 people, with 12% tip.
+# people = int(input("How many people to split the bill between? "))
+# bill_with_tip = tip / 100 * bill + bill
+# tip_as_percentage = tip / 100
+# total_tip_amount = tip_as_percentage * bill
+# total_bill = total_tip_amount + bill
+# print(total_bill)
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+# bill_per_person = total_bill/people
+# final_amount = round(bill_per_person, 2)
+# print(f"Each person should pay: ${final_amount}")
+#Format the result to 2 decimal places = 33.60
+#Tip: You might need to do some research in Google to figure out how to do this.
--- practice.py
+++ practice.py
@@ -3,7 +3,7 @@
 #    print ("Attempt", number , (number ) * ".", "imtiaz")
 
 '''For else loop'''
-#successful = True
+# successful = True
 # for number in range(3):
 #    print("Attempt")
 #    if successful:
@@ -31,14 +31,14 @@
 '''While Loops'''
 # While loop is used to run something/iterate over iterable objects as long as the condition is true
 
-#number = 100
+# number = 100
 # while number > 0:
 #    print(number)
 #    number = number // 2
 #    #number //= 2
 
-#command = ""
-#while command.lower() != "quit":   """This is the better code than the below"""
+# command = ""
+# while command.lower() != "quit":   """This is the better code than the below"""
 # while command != "quit" and  command != "QUIT":
 #    command = input(">>")
 #    print("ECHO", command)
@@ -55,7 +55,7 @@
 
 '''Conditional Statements'''
 
-#temperature = 17
+# temperature = 17
 # if temperature > 30:
 #    print('it is warm')
 #    print("Drink water")
@@ -66,14 +66,14 @@
 # print("Done")
 
 '''Ternary Operator'''
-#age = 12
+# age = 12
 # if age >=18:
 #    print("Eligible")
 # else:
 #    print("Not eligible")
 
 # A slight better code
-#age = 20
+# age = 20
 # if age >= 18:
 #    message ="Eligible"
 # else:
@@ -81,16 +81,16 @@
 # print(message)
 
 # The best code/Ternary code
-#age = 21
-#message = "Eligible" if age >= 18 else "Not eligible"
+# age = 21
+# message = "Eligible" if age >= 18 else "Not eligible"
 # print(message)
 
 '''Logical Operators'''
 # and or not
 
-#high_income = False
-#good_credit = True
-#student = False
+# high_income = False
+# good_credit = True
+# student = False
 
 # if high_income and good_credit:
 #    print("Eligible")
@@ -104,9 +104,9 @@
 
 
 '''Short-circuit Evaluation'''
-#high_income = True
-#good_credit = True
-#student = True
+# high_income = True
+# good_credit = True
+# student = True
 
 # if(high_income and good_credit) and not student:
 #    print("Eligible")
@@ -115,11 +115,11 @@
 
 '''Quiz'''
 
-#if 10 == '10':
+# if 10 == '10':
 #    print('a')
-#elif 'bag' > 'apple' and 'bag' > 'cat':
+# elif 'bag' > 'apple' and 'bag' > 'cat':
 #    print('b')
-#else:
+# else:
 #    print('c')
 
 '''Strings'''
@@ -134,10 +134,10 @@
 # print(course[:])  # Print the whole string from the beginning to the end
 
 '''Escape Sequence'''
-#\"
-#\'
-#\\
-#\n              this will create a newline
+# \"
+# \'
+# \\
+# \n              this will create a newline
 
 # course = "Python \'Programming'"
 # print(course)
@@ -177,7 +177,7 @@
 # print(9/7)   # Float division
 # print(9//7)  # Integer division
 # print(9 % 7)  # Modulus that will find the remainder
-# print(9 ** 7)  # exponential 
+# print(9 ** 7)  # exponential
 
 # x = 10
 # x = x + 3
@@ -187,7 +187,7 @@
 ''' Working with Numbers '''
 
 # import math
-# print (round(2.9))  # rounding up 
+# print (round(2.9))  # rounding up
 # print (abs(-2.9))  # absolute value of a number
 # print(math.ceil(2.9))  # rounding up
 
@@ -209,15 +209,170 @@
 # what are the primitive types in python?
 # 1. Strings
 # 2. Numbers
-# 3. Boolean 
+# 3. Boolean
 # fruit = "Apple"
 # print(fruit[1:-1])
 # print(bool("False"))
 
 '''Exercise'''
-count = 0
-for number in range(1,10):
-    if number % 2 == 0:
-        print(number)
-        count += 1
-print(f"We have {count} even number")
+# count = 0
+# for number in range(1,10):
+#     if number % 2 == 0:
+#         print(number)
+#         count += 1
+# print(f"We have {count} even number")
+
+''' Defining Functions '''
+# def greet():
+#     print("Hi there")
+#     print("Welcome aboard")
+# greet()
+
+''' Arguments '''
+# A parameter is the input that we define for a function whereas an argument is the actual value for a given parameter
+
+# def greet(first_name, last_name):
+    # print(f"Hi {first_name} {last_name}")
+    # print("Welcome aboard")
+#
+#
+# greet("Imtiaz", "Ahmed")
+# greet("John", "Smith")
+
+''' Types of Functions '''
+
+# 1- Perform a task           print()
+# 2- Return a value           round()
+
+# def greet(name):
+#     print(f"Hi {name}")
+    
+# def get_greeting(name):
+#     return f"Hi {name}"
+
+# message = get_greeting("Imtiaz")
+# #print(message)
+# file = open("content.txt", "w")
+# file.write(message)
+
+''' Keyword Arguments '''
+
+# def increment(number, by):
+#     return number + by
+
+# result = increment(2,1)
+# print(result)
+# print(increment(13,by=20))
+
+
+""" Default Arguments """
+
+
+# def increment(number, by=1):
+#     return number + by
+
+# print(increment(13, 20))
+
+''' xargs '''
+
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+
+# print(multiply(2,3,4,5))
+
+# def multiply(x,y,z):
+#     total =  x * y * z
+#     print(total)
+
+# multiply(2,3,14)
+
+""" Key value Pairs/dictionary xxargs """
+
+# def save_user(**user):
+#     print(user["name"])
+#     print(user)
+    
+# save_user(id=1, name="Imtiaz", age=27)
+
+''' Scope '''
+# Scope refers to the region of the code where the variable is defined and if we try to access it outside of it's scope, it will not work.
+
+# def greet():
+#     message = "a"
+#     print(message)
+    
+# greet()
+
+# def greet(name):
+
+#     name = "Imtiaz"
+#     print(name)
+
+# greet("i")
+
+''' Debugging '''
+
+# def multiply(*numbers):
+#     total = 1        # global variable
+#     for number in numbers:
+#         print(number)
+#         total = total * number
+#     return total
+# print("Start")
+
+# print(multiply(4,5,6))
+
+""" Sorthing Lists """
+# numbers = [3,41,45,50,2,4]
+#numbers.sort(reverse = True)
+# print(numbers)
+# print(sorted(numbers, reverse = True))
+
+# items = [
+    # ("product1", 20),
+    # ("product2", 30),
+    # ("product3", 10),
+# ]
+# 
+# def sort_item(item):
+    # return item[1]
+# 
+# items.sort(key = sort_item)
+# print(items)
+
+""" Swapping Variables """
+
+# a = 10
+# b = 20
+
+# c = a  # c = 10
+# a = b  # a = 20
+# b = c  # b = 10
+# Easy method of swapping variables only in python
+# a, b = b, a     # defining a tuple and unpacking it at the same time
+
+# print("a: " , a)
+# print("b: " , b)
+
+""" Arrays """
+
+# from array import array
+# numbers = array("i", [1, 2, 3])
+# numbers.append(4)
+# numbers.pop(3)
+# numbers.insert(5,5)
+# print(numbers)
+# print(numbers[0])
+
+""" Dictionary Comprehensions """
+
+# acc = [x * 2 for x in range(5)]   # Lists
+# print("Looping through a list ", acc)
+# acc = { x * 2 for x in range(5)}  # Sets
+# print("Looping through a set ", acc)
+# acc = { x: x * 2 for x in range(5)}  # Dictionaries
+# print("Looping through a dictionary while assigning a value to a key (key value pairs implemention) ", acc)
+
