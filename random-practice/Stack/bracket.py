9a8e81ca207d76c151bd807593d4a2ecfe0f77f2
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2021-02-23 18:30:27 -0500

    update

--- random-practice/Stack/bracket.py
+++ random-practice/Stack/bracket.py
@@ -1,3 +1,18 @@
+from stack import Stack
+
+
+def is_match(p1, p2):
+    if p1 == "(" and p2 == ")":
+        return True
+    elif p1 == "{" and p2 == "}":
+        return True
+    elif p1 == "[" and p2 == "]":
+        return True
+    else:
+        return False
+
+
+
 def is_paren_balanced(paren_string):
     s = Stack()
     is_balanced = True
